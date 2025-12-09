9fd7d51aa05660a8c0d9f0cba75ac0ebdb978acf
clebson <clebsonsantossouza038@gmail.com>
2025-12-09 11:03:15 -0300

    fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,70 @@
-// === FUNÇÃO PARA TRATAR O CADASTRO ===
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true, 
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule, FooterComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  
+  // === VARIÁVEIS DE LOGIN ===
+  nome = '';
+  senha = '';
+  dataAtual = new Date();
+
+  // === NOVO: VARIÁVEL DE CONTROLE DA TELA ===
+  isLogin: boolean = true; // TRUE = Mostra Login; FALSE = Mostra Cadastro
+
+  // === NOVO: OBJETO PARA O CADASTRO ===
+  registro = {
+    nomeCompleto: '',
+    email: '',
+    senha: '',
+    confirmacaoSenha: ''
+  };
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  // Controla se mostra ou esconde a senha
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  // === NOVO: FUNÇÃO PARA ALTERNAR ENTRE LOGIN E CADASTRO ===
+  toggleMode() {
+    this.isLogin = !this.isLogin; 
+    
+    // Limpa os dados dos formulários ao trocar de tela (melhora a UX)
+    this.nome = '';
+    this.senha = '';
+    this.registro = { nomeCompleto: '', email: '', senha: '', confirmacaoSenha: '' };
+  }
+
+  // Função de login existente
+  login() {
+    console.log('Tentativa de Login:', this.nome);
+    if (this.nome !== 'admin' || this.senha !== '123456') {
+      alert('Nome ou senha Invalidos');
+    } else {
+      this.auth.login();
+      this.router.navigate(['/home']);
+    }
+  }
+
+  // === FUNÇÃO PARA TRATAR O CADASTRO ===
   register() {
     console.log('Dados de Cadastro:', this.registro);
     
@@ -35,3 +101,4 @@
     // LINHA QUE FAZ VOLTAR PARA A TELA DE LOGIN AUTOMATICAMENTE
     this.toggleMode(); 
   }
+}
